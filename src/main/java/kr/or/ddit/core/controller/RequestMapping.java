fd15d4c030ab641516014f8cc0cb515353de2554
juhyung <wthswngud1@naver.com>
2019-06-15 13:16:42 +0900

    mvc 수업중...

---
+++ src/main/java/kr/or/ddit/core/controller/RequestMapping.java
@@ -0,0 +1,10 @@
+package kr.or.ddit.core.controller;
+
+import kr.or.ddit.controller.Controller;
+
+public class RequestMapping {
+
+	public static Controller getController(String uri) {
+		return null;
+	}
+}
